d2c6c9437c619ab5e14e092361ff295bec99a93b
Samuel Legrain <samuel.legrain@bstorm.be>
2024-07-29 16:26:01 +0200

    Add Formulaire

--- Demo01/Controllers/AuthController.cs
+++ Demo01/Controllers/AuthController.cs
@@ -11,12 +11,12 @@ namespace Demo01.Controllers
         {
             return RedirectToAction(nameof(Login));
         }
-
+        [Route("SeConnecter")]
         public IActionResult Login()
         {
             return View();
         }
-
+        [Route("SeConnecter")]
         [HttpPost]
         public IActionResult Login(AuthLoginForm form)
         {
@@ -27,18 +27,18 @@ namespace Demo01.Controllers
                 return RedirectToAction(nameof(Index), "Student");
             }
             ViewData["notification"] = "Connection échouée";
-            return View();
+            return View(form);
         }
 
         [NonAction]
         private static void ValidationLogin(AuthLoginForm form, ModelStateDictionary modelState)
         {
-            modelState.IsRequired(form.Login, nameof(form.Login));
-            modelState.HasMinLenght(form.Login, nameof(form.Login), 2);
-            modelState.HasMaxLenght(form.Login, nameof(form.Login), 6);
-            modelState.IsRequired(form.Password, nameof(form.Password));
-            modelState.HasMinLenght(form.Password, nameof(form.Password), 8);
-            modelState.HasMaxLenght(form.Password, nameof(form.Password), 64);
+            //modelState.IsRequired(form.Login, nameof(form.Login));
+            //modelState.HasMinLenght(form.Login, nameof(form.Login), 2);
+            //modelState.HasMaxLenght(form.Login, nameof(form.Login), 6);
+            //modelState.IsRequired(form.Password, nameof(form.Password));
+            //modelState.HasMinLenght(form.Password, nameof(form.Password), 8);
+            //modelState.HasMaxLenght(form.Password, nameof(form.Password), 64);
             modelState.IsMatched(form.Password, nameof(form.Password), @"[0-9]+", "Le champ \"{0}\" doit contenir un numéro.");
             modelState.IsMatched(form.Password, nameof(form.Password), @"[a-z]+", "Le champ \"{0}\" doit contenir une minuscule.");
             modelState.IsMatched(form.Password, nameof(form.Password), @"[A-Z]+", "Le champ \"{0}\" doit contenir une majuscule.");
--- Demo01/Models/AuthLoginForm.cs
+++ Demo01/Models/AuthLoginForm.cs
@@ -1,8 +1,22 @@
-﻿namespace Demo01.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Demo01.Models
 {
     public class AuthLoginForm
     {
+        [DisplayName("Votre login :")]
+        [Required(ErrorMessage = "Le champs 'Login' est obligatoire")]
+        [StringLength(6,MinimumLength = 2, ErrorMessage = "Le champs 'Login' doit avoir un nombre de caractères entre 2 et 6")]
         public string Login { get; set; }
+        [DisplayName("Votre mot de passe :")]
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Le champs 'Mot de passe' est obligatoire")]
+        [RegularExpression("[0-9]+",ErrorMessage = "Manque un chiffre")]
+        [MinLength(8,ErrorMessage ="Le champ 'Mot de passe' doit avoir au minimum 8 caractères.")]
+        [MaxLength(64,ErrorMessage = "Le champ 'Mot de passe' doit avoir au maximum 64 caractères.")]
+        //[RegularExpression("[0-9]+")]
+        //[RegularExpression("[a-z]+")] ATTENTION : Pas possible de multiplier les attributs, sur un même éléments (Propriétés, Méthodes, champs,...)
         public string Password { get; set; }
     }
 }
