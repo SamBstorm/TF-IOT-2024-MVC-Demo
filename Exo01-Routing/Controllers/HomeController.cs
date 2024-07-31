a16b5f464e121a46732a0ffd7b4b42fe2e21923e
Samuel Legrain <samuel.legrain@bstorm.be>
2024-07-31 12:20:18 +0200

    Add Correction Formulaire + HtmlHelpers

--- Exo01-Routing/Controllers/HomeController.cs
+++ Exo01-Routing/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 using Exo01_Routing.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 namespace Exo01_Routing.Controllers
 {
@@ -28,6 +30,46 @@ namespace Exo01_Routing.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        //Action d'affichage du formulaire : méthode GET
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        //Action de traitement du formulaire : méthode POST
+        [HttpPost]
+        public IActionResult Register(HomeRegisterForm form)
+        {
+            ValidateHomeRegisterForm(form, ModelState);
+            if(!ModelState.IsValid)
+            {
+                return View(form);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        [NonAction]
+        private static void ValidateHomeRegisterForm(HomeRegisterForm form, ModelStateDictionary modelState)
+        {
+
+            if (form.BirthDate > DateOnly.FromDateTime(DateTime.Now.AddYears(-18)))
+            {
+                modelState.AddModelError(nameof(form.BirthDate), "Vous n'êtes pas majeur.");
+            }
+            if(!string.IsNullOrWhiteSpace(form.Password) && !Regex.IsMatch(form.Password, "[a-z]+"))
+            {
+                modelState.AddModelError(nameof(form.Password),"Le mot de passe doit contenir une minuscule.");
+            }
+            if (!string.IsNullOrWhiteSpace(form.Password) && !Regex.IsMatch(form.Password, "[A-Z]+"))
+            {
+                modelState.AddModelError(nameof(form.Password), "Le mot de passe doit contenir une majuscule.");
+            }
+            if (!string.IsNullOrWhiteSpace(form.Password) && !Regex.IsMatch(form.Password, "[0-9]+"))
+            {
+                modelState.AddModelError(nameof(form.Password), "Le mot de passe doit contenir un chiffre.");
+            }
         }
     }
 }
---
+++ Exo01-Routing/Models/HomeRegisterForm.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Exo01_Routing.Models
+{
+    public class HomeRegisterForm
+    {
+        [DisplayName("Nom de famille : ")]
+        [Required(ErrorMessage = "est requis")]
+        [MinLength(4, ErrorMessage = "Pas assez de caractères")]
+        [MaxLength(16, ErrorMessage = "Trop de caractères")]
+        public string Name { get; set; }
+        [DisplayName("Prénom : ")]
+        [Required(ErrorMessage = "est requis")]
+        [MinLength(4, ErrorMessage = "Pas assez de caractères")]
+        [MaxLength(16, ErrorMessage = "Trop de caractères")]
+        public string FirstName { get; set; }
+        [DisplayName("Date de naissance : ")]
+        [DataType(DataType.Date,ErrorMessage = "Veuillez entrer une date valide.")]
+        [Required(ErrorMessage = "est requis")]
+        public DateOnly BirthDate { get; set; }
+        [DisplayName("Pays : ")]
+        public string? Country { get; set; }
+        [DisplayName("Adresse e-mail : ")]
+        [Required(ErrorMessage = "est requis")]
+        [EmailAddress(ErrorMessage = "Ne correspond pas au format d'une adresse e-mail.")]
+        [MinLength(5, ErrorMessage = "Pas assez de caractères")]
+        [MaxLength(320, ErrorMessage = "Trop de caractères")]
+        public string Email { get; set; }
+        [DisplayName("Mot de passe : ")]
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "est requis")]
+        [MinLength(8, ErrorMessage = "Pas assez de caractères")]
+        [MaxLength(64, ErrorMessage = "Trop de caractères")]
+        public string Password { get; set; }
+        [DisplayName("Veuillez confirmer le mot de passe : ")]
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "est requis")]
+        [MinLength(8, ErrorMessage = "Pas assez de caractères")]
+        [MaxLength(64, ErrorMessage = "Trop de caractères")]
+        [Compare(nameof(Password), ErrorMessage = "Les mots de passe ne corespondent pas.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
